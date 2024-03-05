ec7e788c5f59270742c6250bba05236d7d271490
Wiktoria Jeleniewicz <wj@visla.co>
2024-03-05 19:02:07 +0100

    Displaying all products

--- app/src/main/java/com/example/juiceshop/ui/start/LoadingFragment.kt
+++ app/src/main/java/com/example/juiceshop/ui/start/LoadingFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.juiceshop.ApiManager
+import com.example.juiceshop.MainActivity
+import com.example.juiceshop.R
 import com.example.juiceshop.databinding.FragmentLoadingBinding
 
 class LoadingFragment: Fragment() {
@@ -20,14 +22,28 @@ class LoadingFragment: Fragment() {
     ): View {
         binding = FragmentLoadingBinding.inflate(inflater, container, false)
         val root: View = binding!!.root
-        ApiManager.getAllProducts { startMainActivity() }
+        ApiManager.getAllProducts(
+            onSuccess = {
+               json ->
+                json?.let { startMainActivity(it) }
+            }
+        ) { statusCode, message ->
+            startLoadingFailedFragment(statusCode, message)
+        }
 
         return root
     }
 
-    private fun startMainActivity() {
-        var intent = Intent()
+    private fun startMainActivity(json: String) {
+        var intent = Intent(context, MainActivity::class.java)
+        intent.putExtra("ALL_ITEM_LIST", json)
         startActivity(intent)
+    }
+
+    private fun startLoadingFailedFragment(statusCode: Int, message: String) {
+        var fragment = parentFragmentManager?.beginTransaction()
+        fragment?.replace(R.id.nav_host_fragment_activity_loading, LoadingFailedFragment(statusCode, message))
+        fragment?.commit()
     }
 
     override fun onDestroyView() {
